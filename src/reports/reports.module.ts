ed02ee3a7bb8a37374e089a94d1fb074dd42b523
Alexandru Olaru <alexandru.olaru@cellebrite.com>
2022-10-25 13:20:29 +0300

    typeorm

--- src/reports/reports.module.ts
+++ src/reports/reports.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ReportsService } from './reports.service';
 import { ReportsController } from './reports.controller';
+import { Report } from './reports.entity';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([Report]),
+  ],
   providers: [ReportsService],
   controllers: [ReportsController]
 })
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm' 
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { User } from './users.entity';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([User]),
+  ],
   providers: [UsersService],
   controllers: [UsersController]
 })
-export class UserModule {}
+export class UsersModule {}
