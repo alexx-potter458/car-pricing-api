6950f86adba24328c4ed85070a858649557be726
Alexandru Olaru <alexandru.olaru@cellebrite.com>
2022-10-25 16:34:01 +0300

    typeorm

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UsersService } from './users.service';
 
-@Controller('users')
-export class UsersController {}
+@Controller('auth')
+export class UsersController {
+    constructor(private usersService: UsersService){}
+
+    @Post('/signup')
+    createUser(@Body() body: CreateUserDto) {
+        return this.usersService.create(body.email, body.password);
+    }
+
+    @Get('/:id')
+    findOne(){
+        return '';
+    }
+}
