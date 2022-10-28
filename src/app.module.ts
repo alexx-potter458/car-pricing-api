a3122509275f7273183e5fa7658687e13bcbdf23
Alexandru Olaru <alexandru.olaru@cellebrite.com>
2022-10-28 13:57:18 +0300

    Section15

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,17 +7,28 @@ import { ReportsModule } from './reports/reports.module';
 import { UsersModule } from './users/users.module';
 import { User } from './users/users.entity';
 import { Report } from './reports/reports.entity'
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 const cookieSession = require('cookie-session');
+
 @Module({
   imports: [
     UsersModule, 
-    ReportsModule, 
-    TypeOrmModule.forRoot({
-      type: 'sqlite',
-      database: 'db.sqlite',
-      entities: [User, Report],
-      synchronize: true,
+    ReportsModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV}`
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        return {
+          type: 'sqlite',
+          database: config.get<string>('DB_NAME'),
+          entities: [User, Report],
+          synchronize: true,
+        }
+      }    
     })
   ],
   controllers: [AppController],
