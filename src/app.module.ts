4f10f5f0c53eb9b6c7d4b49332fb0c9ceb9cf23c
Alexandru Olaru <alexandru.olaru@cellebrite.com>
2022-10-31 14:49:36 +0200

    ormconf

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ReportsModule } from './reports/reports.module';
 import { UsersModule } from './users/users.module';
-import { User } from './users/users.entity';
-import { Report } from './reports/reports.entity'
+import dbConfig from 'ormconfig';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const cookieSession = require('cookie-session');
 
 @Module({
@@ -19,17 +19,7 @@ const cookieSession = require('cookie-session');
       isGlobal: true,
       envFilePath: `.env.${process.env.NODE_ENV}`
     }),
-    TypeOrmModule.forRootAsync({
-      inject: [ConfigService],
-      useFactory: (config: ConfigService) => {
-        return {
-          type: 'sqlite',
-          database: config.get<string>('DB_NAME'),
-          entities: [User, Report],
-          synchronize: true,
-        }
-      }    
-    })
+    TypeOrmModule.forRoot(dbConfig),
   ],
   controllers: [AppController],
   providers: [
@@ -40,10 +30,13 @@ const cookieSession = require('cookie-session');
     }
   ],
 })
+
 export class AppModule {
+  constructor(private configService: ConfigService) {}
+
   configure(consumer: MiddlewareConsumer) {
     consumer.apply(
-      cookieSession({keys: ['potterhead']})
+      cookieSession({keys: [this.configService.get('COOKIE_KEY')]})
     ).forRoutes('*');
   }
 }
